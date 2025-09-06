2919440aa09e6bceef34ffa31417c2d4bb64f2fc
josepedrolorenzini <webmaster@marbleceramiccorp.com.au>
2025-09-07 02:18:44 +1000

    vue projects

---
+++ frontEndModerno/vue/belRey1/README.md
@@ -0,0 +1,38 @@
+# belRey1
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vite.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+TAREA vue js
+reviar cada uno de ,los ejemplos de vue y conversar con bel acerca de este tema
+
+component cada una de las unidades q bvamos a escribir como vista y estilos
+llamar componentes dentro de otros , llamar componentes padres y hijos
+
+
+
---
+++ frontEndModerno/vue/belRey1/src/components/HelloWorld.vue
@@ -0,0 +1,73 @@
+<script setup>
+import {ref} from 'vue'
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+  greetings: {
+    type: String,
+    required: true,
+  },
+})
+//const greetings = ref("using ref") ;
+const user = {name:"joseph lorenzini" , age:44}
+const onSelect = (e) => {
+  console.log(e)
+}
+
+const count = ref(0) ;
+const increment = () => {
+  count.value++ ;
+}
+
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <p  class="green">{{ greetings }} </p>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <button
+        @click="onSelect"
+        :name="user.name"
+        :age="user.age"
+      >Click me</button>
+      <button
+        @click="increment"
+      >count {{ count }}</button>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+.greetings p {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
